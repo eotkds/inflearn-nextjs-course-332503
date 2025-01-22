022ed0d7b07f08e99337f6c078f8eab15c24e5ee
eotkds <eotkds@gmail.com>
2025-01-22 22:52:17 +0900

    By section 4 - next용 msw 컴포넌트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { MSWProvider } from "./_component/MSWcomponent";
 
+
+if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.NODE_ENV !== 'production') {
+  const { server } = require('@/mocks/http')
+  server.listen()
+}
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <MSWProvider>{children}</MSWProvider>
       </body>
     </html>
   );
