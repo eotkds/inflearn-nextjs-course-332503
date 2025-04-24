5317e10c83fca4ac5daf3bd8e190d8f5ecc4c661
eotkds <eotkds@gmail.com>
2025-04-24 15:28:05 +0900

    study-v2 라우트 그룹 설정, main 페이지설정, 로그인 시 redirect 설정

---
+++ study-v2/src/app/(afterLogin)/home/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage(){
+    return(
+        <div>
+            홈 페이지
+        </div>
+    );
+}
--- study-v2/src/app/layout.tsx
+++ study-v2/src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        메인 레이아웃
         {children}
       </body>
     </html>
